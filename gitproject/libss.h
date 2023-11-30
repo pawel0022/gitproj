f58ee1b66d504ad6e3b3a15c07f2d73faf4f7064
pawel0022 <pawel2002pl@wp.pl>
2023-11-30 23:01:55 +0100

    name

---
+++ gitproject/libss.h
@@ -0,0 +1,5 @@
+#ifndef libs_h
+#define libs_h
+#include <iostream>
+
+#endif libs_h
---
+++ gitproject/main.cpp
@@ -0,0 +1,8 @@
+#include "libss.h"
+
+int main() {
+	std::cout << "start\n";
+
+	system("pause");
+	return 0;
+}
